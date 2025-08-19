4dc1d096a1fd2f4831db91fe5825872f1b59b452
VITOR ALMADA BRILHANTE MENDONCA <vitor.2924@aluno.pr.senac.br>
2025-08-19 09:01:46 -0300

    Colocando o emaill e o olho, ajustando a parte de senha e email

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,30 +2,46 @@ import React from "react";
 import { Text, View, Image, TextInput } from "react-native";
 import { style } from "./styles";
 import Logo from "../../assets/logo.png";
- 
+import { MaterialIcons } from '@expo/vector-icons';
+import { themas } from "../../global/themes";
+
 export default function Login() {
   return (
     <View style={style.container}>
-        <View style={style.boxTop}>
-      <Image
-      source={Logo}
-      style={style.logo}
-      resizeMode="contain"
-      />
-      <Text style={style.text}>Bem vindo de volta!</Text>
-        </View>
-        <View style={style.boxMid}>
-          <Text style={style.titleInput} >ENDEREÇO DE E-MAIL</Text>
-          <View style={style.boxInput}>
-        <TextInput
-            style={style.input}
+      <View style={style.boxTop}>
+        <Image
+          source={Logo}
+          style={style.logo}
+          resizeMode="contain"
         />
-          </View>
-          <Text style={style.titleInput}>Senha</Text>
-          <TextInput />
+        <Text style={style.text}>Bem vindo de volta!</Text>
+      </View>
+      <View style={style.boxMid}>
+        <Text style={style.titleInput} >ENDEREÇO DE E-MAIL</Text>
+        <View style={style.boxInput}>
+          <TextInput
+            style={style.input}
+          />
+          <MaterialIcons
+            name="email"
+            size={20}
+            color={themas.colors.gray}
+          />
         </View>
-        <View style={style.boxBottom}>
-          </View>
+        <Text style={style.titleInput}>SENHA</Text>
+        <View style={style.boxInput}>
+          <TextInput
+            style={style.input}
+          />
+          <MaterialIcons 
+          name="remove-red-eye"
+          size={20}
+          color={themas.colors.gray}
+          />
+        </View>
+      </View>
+      <View style={style.boxBottom}>
+      </View>
     </View>
   );
 }
--- src/pages/login/styles.ts
+++ src/pages/login/styles.ts
@@ -13,17 +13,21 @@ export const style  = StyleSheet.create({
     boxTop: {
         height: Dimensions.get('window').height / 3,
         width: '100%',
-        backgroundColor: 'red'
+        // backgroundColor: 'red'
+        alignItems: 'center',
+        justifyContent: 'center'
     },
     boxMid: {
         height: Dimensions.get('window').height / 4,
         width: '100%',
-        backgroundColor: 'green'
+        // backgroundColor: 'green'
+        padding: 37
     },
     boxBottom: {
         height: Dimensions.get('window').height / 3,
         width: '100%',
-        backgroundColor: 'blue'
+       backgroundColor: 'blue'
+
     },
     logo: {
         width:80,
@@ -46,10 +50,15 @@ export const style  = StyleSheet.create({
         borderRadius: 40,
         marginTop: 10,
         flexDirection: 'row',
+        alignItems: 'center',
+        paddingHorizontal: 5,
+        backgroundColor: themas.colors.lightGray,
+        borderColor: themas.colors.lightGray
     },
     input: {
         height: '100%',
-        width: '100%',
-        backgroundColor: 'red',
+        width: '90%',
+        // backgroundColor: 'red',
+        borderRadius: 40
     }
 })
