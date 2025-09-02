1c2d6a5c42e493b995082cf20b911ba44c24cf12
VITOR ALMADA BRILHANTE MENDONCA <vitor.2924@aluno.pr.senac.br>
2025-09-02 08:56:06 -0300

    Deixando a posição dos ícones mais responsivo, colocando propriedades para que não haja quebra no input

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,20 +1,53 @@
-import React, { forwardRef } from "react";
-import { View, Text, TextInput } from 'react-native';
+import React, { ForwardedRef, forwardRef, Fragment } from "react";
+import { View, Text, TextInput, TextInputProps } from 'react-native';
 import { style } from "../../pages/login/styles";
-import { MaterialIcons } from '@expo/vector-icons';
+// import { MaterialIcons } from '@expo/vector-icons';
 import { themas } from "../../global/themes";
+import {FontAwesome, MaterialIcons, Octicons} from '@expo/vector-icons';
 
-export const Input = forwardRef(() => {
+
+type IconComponent = React.ComponentType<React.ComponentProps<typeof MaterialIcons>> |
+React.ComponentType<React.ComponentProps<typeof FontAwesome>> |
+React.ComponentType<React.ComponentProps<typeof Octicons>>;
+
+type Props = TextInputProps & {
+    IconLeft?: IconComponent, //Icones ativos
+    IconRight?: IconComponent,
+    IconLeftName?: string,
+    IconRightName?: string,
+    title?: string,
+    OnIconLeftPress?: () => void, //Ação do ícone
+    OnIconRightPress?: () => void 
+}
+
+
+
+export const Input = forwardRef<TextInput, Props>((Props, ref: ForwardedRef<TextInput> | null) => {
+    const {IconLeft, IconRight, title, OnIconLeftPress, OnIconRightPress,
+        ...reset
+    } = Props
+    const calculateSizeWidth = () => {
+        if (IconLeft && IconRight) {
+            return '100%'
+        } else if (IconLeft || IconRight) {
+            return '100%'
+        } else {
+            return '100%'
+        }
+    }
     return (
+        <>
+        <Text style ={style.titleInput}>ENDEREÇO DE E-MAIL</Text>
         <View style={style.boxInput}>
             <TextInput
                 style={style.input}
-            />
-            <MaterialIcons
-                name="email"
-                size={20}
-                color={themas.colors.gray}
-            />
+        />
+        <MaterialIcons
+            name="email"
+            size={20}
+            color={themas.colors.gray}
+        />
         </View>
+        </>
     )
 })
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,91 +1,104 @@
-import React, {useState} from "react";
-import { Text, View, Image, TextInput, TouchableOpacity, Alert, ActivityIndicator } from "react-native";
+import React, { useState } from "react";
+
+import {
+    Text, View, Image, TextInput, TouchableOpacity,
+    Alert, ActivityIndicator
+} from "react-native";
 import { style } from "./styles";
 import Logo from "../../assets/logo.png";
-import { MaterialIcons } from '@expo/vector-icons';
-import { themas } from "../../global/themes";
-import {Input} from "../../components/input";
-
+import { MaterialIcons, Octicons } from '@expo/vector-icons'
+import { themas } from "../../global/themes"
+import { Input } from "../../components/input";
 
 export default function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [loading, setLoading] = useState(false);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [loading, setLoading] = useState(false);
 
-  async function getLogin() {
-    try {
-      setLoading(true)
-      if(!email || !password) {
-        return Alert.alert('Atenção', 'Informe os campos obrigatórios');
-      }
+    async function getLogin() {
+        try {
+            setLoading(true)
+            if (!email || !password) {
+                return Alert.alert('Atenção', 'Informe os campos obrigatórios!');
+            }
 
-      setTimeout(() => {
-        if(email == 'bjj@gmail.com' && password == '12345678') {
-          Alert.alert('Logado com sucesso!');
-        } else {
-          Alert.alert('Usuário não encontrado!');
+            setTimeout(() => {
+                if (email == 'leo@gmail.com' && password == '12345678') {
+                    Alert.alert('Logado com sucesso!');
+                } else {
+                    Alert.alert('Usuário não encontrado!');
+                }
+                setLoading(false)
+            }, 3000)
+        } catch (error) {
+            console.log(error);
         }
-        setLoading(false)
-
-    }, 3000)
-
-    } catch (error) {
-      console.log(error);
     }
-    
-  }
+    return (
+        < View style={style.container} >
+            <View style={style.boxTop}>
+                <Image
+                    source={Logo}
+                    style={style.logo}
+                    resizeMode="contain"
+                />
+                <Text style={style.text}>Bem vindo de volta!</Text>
+            </View>
+            <View style={style.boxMid}>
+                <Input 
+                value={email}
+                onChangeText={setEmail}
+                title="ENDEREÇO E-MAIL"
+                IconLeft={MaterialIcons}
+                IconLeftName="email"
+                IconRight={MaterialIcons}
+                IconRightName="email"
+                />
+                <Input 
+                value={password}
+                onChangeText={setPassword}
+                title="SENHA"
+                IconRight={Octicons}
+                IconRightName="eye-closed"
+                />
+                {/* <Text style={style.titleInput}>ENDEREÇO DE E-MAIL</Text> */}
 
-  return (
-    <View style={style.container}>
-      <View style={style.boxTop}>
-        <Image
-          source={Logo}
-          style={style.logo}
-          resizeMode="contain"
-        />
-        <Text style={style.text}>Bem vindo de volta!</Text>
-      </View>
-      <View style={style.boxMid}>
-        <Input />
-       {/* <Text style={style.titleInput} >ENDEREÇO DE E-MAIL</Text>
-         <View style={style.boxInput}>
-          <TextInput
-            style={style.input}
-            value={email}
-            onChangeText={setEmail}
-
-          />
-          <MaterialIcons
-            name="email"
-            size={20}
-            color={themas.colors.gray}
-          />
-        </View>
-        <Text style={style.titleInput}>SENHA</Text>
-        <View style={style.boxInput}>
-          <TextInput
-            style={style.input}
-            value={password}
-            onChangeText={setPassword}
-          />
-          <MaterialIcons
-            name="remove-red-eye"
-            size={20}
-            color={themas.colors.gray}
-          />
-        </View>  */}
-      </View>
-      <View style={style.boxBotton}>
-
-        <TouchableOpacity style={style.button} onPress={() => getLogin()}>
-          {
-            loading ? <ActivityIndicator color={'#ffff'} size={"small"}/> : 
-            <Text style={style.textButton}>Entrar</Text>
-          }
-
-        </TouchableOpacity>
-      </View>
-      <Text style={style.textBotton}>Não tem conta? <Text style={{color: themas.colors.primary}}>Crie agora!</Text></Text>
-    </View>
-  );
+                {/* <View style={style.boxInput}>
+                    <TextInput
+                        style={style.input}
+                        value={email}
+                        onChangeText={setEmail}
+                    />
+                    <MaterialIcons
+                        name="email"
+                        size={20}
+                        color={themas.colors.gray}
+                    />
+                </View>
+                <Text style={style.titleInput}>SENHA</Text>
+                <View style={style.boxInput}>
+                    <TextInput
+                        style={style.input}
+                        value={password}
+                        onChangeText={setPassword}
+                    />
+                    <MaterialIcons
+                        name="remove-red-eye"
+                        size={20}
+                        color={themas.colors.gray}
+                    />
+                </View> */}
+            </View>
+            <View style={style.boxBotton}>
+                <TouchableOpacity style={style.button} onPress={() => getLogin()}>
+                    {
+                        loading ? <ActivityIndicator color={'#ffff'} size={"small"} /> :
+                            <Text style={style.textButton}>Entrar</Text>
+                    }
+                </TouchableOpacity>
+            </View>
+            <Text style={style.textBotton}>Não tem conta?
+                <Text style={{ color: themas.colors.primary }}> Crie agora!</Text></Text>
+        </View >
+    )
 }
