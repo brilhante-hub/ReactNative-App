14f09b4b819bb2f20a49da3e08adfcd766d07dab
VITOR ALMADA BRILHANTE MENDONCA <vitor.2924@aluno.pr.senac.br>
2025-09-18 08:06:53 -0300

    Criando o header da Lista, adicionando estilos, text e style para header

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -1,11 +1,22 @@
 import React from "react";
-
 import { Text, View } from "react-native";
+import { style } from "./styles";
+import { Input } from "../../components/input";
+import {MaterialIcons} from '@expo/vector-icons';
 
-export default function List () {
+export default function List() {
     return (
-            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}> 
-            <Text>Lista</Text>
+        <View style={style.container}>
+            <View style={style.header}>
+                <Text style={style.greeting}>Seja bem vindo!
+                    <Text style={{fontWeight: 'bold', color: 'black'}}> Vitor Almada Brilhante</Text></Text>
+                    <View style={style.boxInput}>
+                        <Input 
+                        IconLeft={MaterialIcons}
+                        IconLeftName="search"
+                        />
+                    </View>
             </View>
+        </View>
     )
 }
