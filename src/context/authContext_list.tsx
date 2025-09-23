d64d6aa05c46bddaffc3b0fc789958506368a7ab
VITOR ALMADA BRILHANTE MENDONCA <vitor.2924@aluno.pr.senac.br>
2025-09-23 08:43:16 -0300

    Arrumando modal, aplicando style dentro do próprio arquivo index, além de também colocar as funcionalidades no list e user, também colocando isso dentro do modal

--- src/context/authContext_list.tsx
+++ src/context/authContext_list.tsx
@@ -1,8 +1,9 @@
-import React, {createContext, useContext, useRef} from "react";
-import { Dimensions, Text } from "react-native";
+import React, { createContext, useContext, useRef } from "react";
+import { Dimensions, Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { MaterialIcons, AntDesign } from '@expo/vector-icons';
 import { Modalize } from "react-native-modalize";
 
-export const AuthContextList : any = createContext({});
+export const AuthContextList: any = createContext({});
 
 export const AuthProviderList = (props: any): any => {
 
@@ -13,19 +14,47 @@ export const AuthProviderList = (props: any): any => {
 
     }
     const _container = () => {
-        return <Text></Text>
+        return (
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <TouchableOpacity>
+                        <MaterialIcons
+                            name="close"
+                            size={30}
+
+                        />
+                    </TouchableOpacity>
+                    <Text>Criar tarefa</Text>
+                    <TouchableOpacity>
+                        <AntDesign
+                            name="check"
+                            size={30}
+
+                        />
+                    </TouchableOpacity>
+
+
+                </View>
+            </View>
+        )
     }
-    return(
+    return (
         <AuthContextList.Provider value={{ onOpen }}>
             {props.children}
-            <Modalize 
-            ref={modalizeRef}
-            modalHeight={Dimensions.get('window').height / 1.3}
-            
-            />
-            {}
+            <Modalize
+                ref={modalizeRef}
+                // modalHeight={Dimensions.get('window').height / 1.3}
+                childrenStyle={{height: Dimensions.get('window').height / 1.3 }}
+                adjustToContentHeight={true}
+
+            >
+            {_container()}
+            </Modalize>
         </AuthContextList.Provider>
     )
 }
 
 export const useAuth = () => useContext(AuthContextList);
+export const styles = StyleSheet.create({
+
+})
